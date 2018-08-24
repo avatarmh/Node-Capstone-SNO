e1956fda030285e81fb4b7bc57b5f388334661d1
Miriam Heller <miriam.heller@gmail.com>
2018-08-24 11:12:06 -0400

    No accessibility violations found in the current state of the page with AXE; need to add functionality to submit button

--- public/js/joinSNO.js
+++ public/js/joinSNO.js
@@ -4,4 +4,3 @@ dropdownCountries = window.countries.map(countryData => {
 
 $("#country-dropdown").append(dropdownCountries);
 
-<input type="button" onclick="location.href='/login.html';" value="Go to Login" />
