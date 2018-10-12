2e92bc94fa0349ea0b24d07f7fec514675f1f1f2
Sebastian Bastidas <sbastidasr@gmail.com>
2018-10-12 23:13:52 +0200

    PUT/POST

--- public/js/edit-member-news.js
+++ public/js/edit-member-news.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   let searchParams = new URLSearchParams(window.location.search)
   let id = searchParams.get('id')
 
-  
-  
+
+
   const url = `api/news/${id}`
   $.ajax({
     url,
@@ -19,14 +19,14 @@ $(document).ready(function() {
     }
   });
 
-  
+
 
   function fillFormInputsWithItem(item) {
-      let formattedDate = moment(item.created).format('MM/DD/YYYY');
-      $(".title").val(item.title);
-      $(".date").val(formattedDate);
-      $(".source").val(item.source);
-      $(".summary").val(item.summary);
+    let formattedDate = moment(item.created).format('MM/DD/YYYY');
+    $(".title").val(item.title);
+    $(".date").val(formattedDate);
+    $(".source").val(item.source);
+    $(".summary").val(item.summary);
   }
 
   $(".edit-form").submit((event) => {
@@ -36,14 +36,27 @@ $(document).ready(function() {
       title: $("input[name=title]").val(),
       date: $("input[name=date]").val(),
       source: $("input[name=source]").val(),
-      summary: $("textarea[name=summary]").val()      
-      } 
+      summary: $("textarea[name=summary]").val()
+    }
+    $.ajax({
+      url,
+      method: id? "PUT" : "POST",
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify(newsDetails),
+      success: (data) => {
+        console.log(data)
+        // NEWS SUCCESFULLY CREATED
+        window.location.href = "/member-news.html"
+      },
+      error: (error) => {
+        console.log(error);
+      }
     });
+
+  });
+
+
+
+  //
 })
-
-
-
-
-
-
-
