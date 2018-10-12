9e1ffed3b5101b5dfaa9bac0d0178f6275be6ca3
Sebastian Bastidas <sbastidasr@gmail.com>
2018-10-12 23:37:35 +0200

    FixEdit

--- public/js/edit-member-news.js
+++ public/js/edit-member-news.js
@@ -29,16 +29,21 @@ $(document).ready(function() {
     $(".summary").val(item.summary);
   }
 
-  $(".edit-form").submit((event) => {
+console.log("--")
+  $(".edit-form").submit(event => {
+    console.log("HI")
     event.preventDefault()
     const url = "/api/news/";
-    const slug = id ? id : "",
+    const slug = id ? id : "";
     const newsDetails = {
-      title: $("input[name=title]").val(),
+      title: $("textarea[name=title]").val(),
       date: $("input[name=date]").val(),
       source: $("input[name=source]").val(),
-      summary: $("textarea[name=summary]").val()
+      summary: $("textarea[name=summary]").val(),
+      id
     }
+
+      console.log(newsDetails)
     $.ajax({
       url:url+slug,
       method: id? "PUT" : "POST",
