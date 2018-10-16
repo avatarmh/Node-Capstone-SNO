bf67f83b876d2d01a3d6e9ea1fdec4ff2d7b5116
Miriam Heller <miriam.heller@gmail.com>
2018-10-16 17:15:58 -0400

    fixed login and square-plus icon for add news; still needs formatting

--- public/js/member-directory.js
+++ public/js/member-directory.js
@@ -16,9 +16,25 @@ $(document).ready(function() {
       }
     });
 
-function generateMember(member) {
-  return `
-      <div class='member-info'>
+  function sortByKey(array, key) {
+    return array.sort(function (a, b) {
+      var x = a[key];
+      var y = b[key];
+
+      if (typeof x == "string") {
+        x = ("" + x).toLowerCase();
+      }
+      if (typeof y == "string") {
+        y = ("" + y).toLowerCase();
+      }
+
+      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+  }
+
+  function generateMember(member) {
+    return `
+        <div class='member-info'>
       <h2>${member.firstName} ${member.lastName}</h2>
        <p>Affiliation: ${member.affiliation}</p>
        <p>Position: ${member.position}</p>
@@ -27,6 +43,8 @@ function generateMember(member) {
 }
 
     function generateMembersString(memberList) {
+      // console.log(memberList)
+      // sortByKey(memberList, lastName);
       const members = memberList.map(member=> generateMember(member));
       return members.join("");
     }
