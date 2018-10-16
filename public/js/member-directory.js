8ba545281d0e9f239155064827da929c5ac7cfc2
Miriam Heller <miriam.heller@gmail.com>
2018-10-16 16:17:47 -0400

    add member directory formatted

---
+++ public/js/member-directory.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  const url = "api/users/"
+  console.log('in memberdirectory js')
+  $.ajax({
+      url,
+      method: 'GET',
+      dataType: 'json',
+      contentType: 'application/json',
+      success: (data) => {
+        console.log(data);
+         const results = generateMembersString(data);
+        $('.members').html(results);
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+
+function generateMember(member) {
+  return `
+      <div class='member-info'>
+      <h2>${member.firstName} ${member.lastName}</h2>
+       <p>Affiliation: ${member.affiliation}</p>
+       <p>Position: ${member.position}</p>
+       <p>Research focus: ${member.researchFocus}</p>
+      </div>`
+}
+
+    function generateMembersString(memberList) {
+      const members = memberList.map(member=> generateMember(member));
+      return members.join("");
+    }
+  
+  
+})
