7d302839ac62d1031a0e2b880fc65b84c2e3c1dc
mheller8 <miriam.heller@gmail.com>
2018-12-06 15:55:59 -0500

    added 2 demo news items

--- auth/records.js
+++ auth/records.js
@@ -1,15 +1,15 @@
 module.exports = [
     {
-        title: "title",
+        title: "Artificial synapses made from nanowires",
         date: "12/5/2018",
-        source: "source",
-        summary: "summary",
+        source: "Forschungszentrum Juelich",
+        summary: "Scientists have produced a memristive element made from nanowires that functions in much the same way as a biological nerve cell. The component is able to both save and process information, as well as receive numerous signals in parallel. The resistive switching cell made from oxide crystal nanowires is thus proving to be the ideal candidate for use in building bioinspired 'neuromorphic' processors.",
         ownerID: "5bfdc1d20227b938b4ac8f69"},
     {
-        title: "title2",
-        date: "12/5/2018",
-        source: "source2",
-        summary: "summary2",
+        title: "Colloidal quantum dots make LEDs shine bright in the infrared",
+        date: "11/3/2018",
+        source: "ICFO-The Institute of Photonic Sciences",
+        summary: "A group of researchers report on the development of a colloidal quantum-dot light emitting diode with unprecedented quantum and power conversion efficiencies in the infrared range. The study has proven that these devices can also be integrated in inorganic solar cells and may lead to even higher efficiencies.",
         ownerID: "5bfdc1d20227b938b4ac8f69"
       }
 ]
